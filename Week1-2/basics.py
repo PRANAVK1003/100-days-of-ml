e87130ae6d361005499fc34bf504d175122e737b
PRANAVK <94790325+PRANAVK1003@users.noreply.github.com>
2024-07-18 23:48:36 +0530

    Add files via upload

---
+++ Week1-2/basics.py
@@ -0,0 +1,47 @@
+#variables : containers for storing data 
+x = 5 
+print(x)
+#data types : diff type for storing variables 
+a = 9.8 
+print(type(a))
+b = True
+print(type(b)) 
+c = "Python"
+print(type(c))
+
+#Operations : Variables used with each other . 
+num1 = 20.5 
+num2 = 10.5 
+num3 = num1 + num2 
+print(num3)
+
+#Lists : Ordered , mutable collection of values enclosed by square brackets . 
+List = ["Pranav" , 21 , 179 , 'Indian']
+print(List)
+len(List)
+List.append(75)
+print(List)
+print(len(List))
+#Changing the value 
+List[1]=20
+print(List)
+
+
+#Tuples : Immutables - Values are never changed . 
+tuples = ('Kendrick Lamar' ,37, "America")
+print(tuples)
+tuples = tuples  + (1987, "Not Like Us")
+print(tuples)
+
+#Sets : Unordered & mutable . Every item is unique . 
+sets = "Python is the most versatile language."
+print(set(sets))
+print(set(sets.split(" ")))
+
+#Indexing : 
+
+
+
+
+
+ 
