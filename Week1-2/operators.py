f38d0d924435ce452cf0aac9a7e86158f575f684
PRANAVK <94790325+PRANAVK1003@users.noreply.github.com>
2024-07-21 08:29:56 +0530

    Add files via upload

---
+++ Week1-2/operators.py
@@ -0,0 +1,46 @@
+#Arithmetic operators
+"""x=10
+y=5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)"""
+
+#Assignment Operators 
+x = 5
+x+=3 #x=x+3
+x-=1
+x*=50
+x/=10
+x**=5
+x%=2
+x//4
+print(x)
+#Comparison operators 
+x=5
+y=3
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+#Logical Operators 
+print(10>2 and 2<5)
+print(10>2 or 2<10)
+print(not(10>2))
+#Bitwise Operators
+x=90
+y=100
+print(x&y)
+print(x|y)
+print(x^y)
+print(~x)
+print(x<<y)
+print(x>>y)
+
+
+
